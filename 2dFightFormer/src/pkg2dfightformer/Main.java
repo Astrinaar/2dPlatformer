fe781f9f6cffcfef9969017fde724fae154058fc
Astrinaar <7h380x@gmail.com>
2014-08-22 17:05:01 +0200

    created project

---
+++ 2dFightFormer/src/pkg2dfightformer/Main.java
@@ -0,0 +1,17 @@
+
+package pkg2dfightformer;
+
+/**
+ *
+ * @author PK
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
